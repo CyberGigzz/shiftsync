61eac5365726d98c0dec29d791d7d9f416a5c967
Giga <gigakatamadze0@gmail.com>
2025-07-30 16:06:11 +0400

    Initial commit

---
+++ src/main/java/com/epam/shiftsync/ShiftsyncApplication.java
@@ -0,0 +1,13 @@
+package com.epam.shiftsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftsyncApplication.class, args);
+	}
+
+}
